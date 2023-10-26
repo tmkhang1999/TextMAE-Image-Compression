5d4644924215d49dae29df4a4b35a454614ae892
KhoiTrant <85048572+KhoiTrant68@users.noreply.github.com>
2023-10-26 20:08:40 +0700

    test for one image

--- testing.py
+++ testing.py
@@ -1,9 +1,17 @@
 from torchvision import transforms
 from utils.dataloader import get_image_dataset
+from models.Compression.MCM import MCM
 
-trainset = get_image_dataset('kodak', 'crop=224, hlip=True')
+trainset = get_image_dataset('kodak', 'crop=224, hflip=False')
 
-transform = transforms.ToPILImage()
-img = transform(trainset[1][0])
-img.show()
-# print(f'Training transform: \n{str(trainset.transform)}')
+imgs, ori_shape, total_scores = trainset[0]
+model = MCM()
+model.update()
+
+out_enc = model.compress(imgs.unsqueeze(dim=0), total_scores)
+print(out_enc["string"])
+print(out_enc["shape"])
+print(out_enc["ids_restore"].shape)
+out_dec = model.decompress(out_enc["string"], out_enc["shape"], out_enc["ids_restore"])
+print(out_dec['x_hat'].shape)
+
