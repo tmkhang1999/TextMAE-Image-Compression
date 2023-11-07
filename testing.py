62423db0741e19a4c30a7a91ba096b5ffd1c06fa
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2023-11-08 00:13:37 +0700

    Create testing.py with huffman class

--- testing.py
+++ testing.py
@@ -1,32 +1,258 @@
+import argparse
+import json
+import math
+import os
+import sys
+import time
+from collections import defaultdict
+from pathlib import Path
+from typing import Any, Dict, List
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from compressai.ops import compute_padding
+from compressai.zoo import load_state_dict
+from pytorch_mssim import ms_ssim
+from torch.utils.data import DataLoader
 from torchvision import transforms
-from utils.dataloader import get_image_dataset
+
 from models.Compression.MCM import MCM
-from torch.utils.data import DataLoader, SequentialSampler
-from models.Compression.common import logger
+from utils.dataloader import get_image_dataset
+from utils.huffman import HuffmanCoding
 
-if __name__ == '__main__':
-    cfg = {"crop": 224, "hflip": False}
-    test_dataset = get_image_dataset("./datasets/kodak", cfg)
-    sampler_val = SequentialSampler(test_dataset)
+# Constants
+IMG_EXTENSIONS = (".jpg", ".jpeg", ".png", ".ppm", ".bmp", ".pgm", ".tif", ".tiff", ".webp")
 
+# Set deterministic behavior
+torch.backends.cudnn.deterministic = True
+torch.set_num_threads(1)
+
+
+def collect_images(rootpath: str) -> list[Path | None]:
+    image_files = []
+
+    for ext in IMG_EXTENSIONS:
+        image_files.extend(Path(rootpath).rglob(f"*{ext}"))
+    return sorted(image_files)
+
+
+def psnr(a: torch.Tensor, b: torch.Tensor, max_val: int = 255) -> float:
+    return 20 * math.log10(max_val) - 10 * torch.log10((a - b).pow(2).mean())
+
+
+def compute_metrics(org: torch.Tensor, rec: torch.Tensor, max_val: int = 255) -> Dict[str, Any]:
+    org = (org * max_val).clamp(0, max_val).round()
+    rec = (rec * max_val).clamp(0, max_val).round()
+    psnr_value = psnr(org, rec)
+    ms_ssim_value = ms_ssim(org, rec, data_range=max_val)
+    return {"psnr": psnr_value.item(), "ms-ssim": ms_ssim_value.item()}
+
+
+def save_output(x, file_name, output_dir):
+    x = x.squeeze().clamp(0, 1)
+    x = transforms.ToPILImage()(x.cpu())
+    x.save(os.path.join(output_dir, file_name))
+
+
+@torch.no_grad()
+def inference(model, x, total_score, file_name, output_dir):
+    # Create necessary directories
+    os.makedirs(output_dir, exist_ok=True)
+    ids_dir = os.path.join(output_dir, 'bin')
+    os.makedirs(ids_dir, exist_ok=True)
+
+    # Add padding to the input image
+    h, w = x.size(2), x.size(3)
+    pad, unpad = compute_padding(h, w, min_div=2 ** 6)  # Pad to allow 6 strides of 2
+    x_padded = F.pad(x, pad, mode="constant", value=0)
+
+    # Compression process
+    start = time.time()
+    out_enc = model.compress(x_padded, total_score)
+    enc_time = time.time() - start
+
+    # Store the list of IDs for Huffman coding
+    ids_keep = out_enc["ids_keep"]
+    huffman = HuffmanCoding()
+    compressed_ids_keep, device = huffman.compress(ids_keep)
+    decompressed_ids_keep = huffman.decompress(compressed_ids_keep, device)
+
+    # Decompression process
+    start = time.time()
+    out_dec = model.decompress(out_enc["strings"], out_enc["shape"], decompressed_ids_keep)
+    dec_time = time.time() - start
+
+    # Remove padding from the reconstructed image
+    out_dec["x_hat"] = F.pad(out_dec["x_hat"], unpad)
+
+    # Save the reconstructed image
+    save_output(out_dec["x_hat"], file_name, output_dir)
+
+    # Calculate metrics for evaluation
+    metrics = compute_metrics(x, out_dec["x_hat"], 255)
+    num_pixels = x.size(0) * x.size(2) * x.size(3)
+
+    # Calculate bits per pixel (bpp)
+    bpp = sum(len(s[0]) for s in out_enc["strings"]) * 8.0 / num_pixels
+    bpp += (len(compressed_ids_keep) / num_pixels)
+
+    return {
+        "psnr": metrics["psnr"],
+        "ms-ssim": metrics["ms-ssim"],
+        "bpp": bpp,
+        "encoding_time": enc_time,
+        "decoding_time": dec_time,
+    }
+
+
+@torch.no_grad()
+def inference_entropy_estimation(model, x):
+    start = time.time()
+    out = model.forward(x)
+    elapsed_time = time.time() - start
+
+    metrics = compute_metrics(x, out["x_hat"], 255)
+    num_pixels = x.size(0) * x.size(2) * x.size(3)
+    bpp = sum(
+        (torch.log(likelihoods).sum() / (-math.log(2) * num_pixels)) for likelihoods in out["likelihoods"].values())
+
+    return {
+        "psnr": metrics["psnr"],
+        "ms-ssim": metrics["ms-ssim"],
+        "bpp": bpp.item(),
+        "encoding_time": elapsed_time / 2.0,  # broad estimation
+        "decoding_time": elapsed_time / 2.0,
+    }
+
+
+def load_checkpoint(num_keep_patches: int, checkpoint_path: str) -> nn.Module:
+    state_dict = load_state_dict(torch.load(checkpoint_path)['model'])
+    return MCM().from_state_dict(num_keep_patches, state_dict).eval()
+
+
+def eval_model(
+        model: nn.Module,
+        output_dir: Path,
+        input_dir: Path,
+        filepaths: List[Path],
+        args,
+) -> Dict[str, Any]:
+    device = next(model.parameters()).device
+    metrics = defaultdict(float)
+
+    test_dataset = get_image_dataset(is_train=False,
+                                     dataset_path=input_dir,
+                                     args=args)
     test_dataloader = DataLoader(
         test_dataset,
         sampler=None,
         batch_size=1,
         num_workers=1,
         pin_memory=True,
-        drop_last=False)
+        drop_last=False
+    )
 
-    model = MCM()
-    model.update()
-    for (imgs, ori_shapes, total_scores) in test_dataloader:
-        out = model(imgs, total_scores)
+    for index, (img, ori_shape, total_score) in enumerate(test_dataloader):
+        file_name = str(filepaths[index]).split("/")[-1]
+        file_name = file_name.split(".")[0]
+        if not args.entropy_estimation:
+            if args.half:
+                model = model.half()
+                img = img.to(device).half()
+            rv = inference(model, img, total_score, file_name, output_dir)
+        else:
+            rv = inference_entropy_estimation(model, img)
+        for k, v in rv.items():
+            metrics[k] += v
 
-        out_enc = model.compress(imgs, total_scores)
-        print(len(out_enc["string"][0]), len(out_enc["string"][1]))
-        print(out_enc["shape"])  
-        print(out_enc["ids_restore"].shape)
-        out_dec = model.decompress(
-            out_enc["string"], out_enc["shape"], out_enc["ids_restore"])
-        print(out_dec["x_hat"].shape)
+    for k, v in metrics.items():
+        metrics[k] = v / len(filepaths)
 
+    return metrics
+
+
+def setup_args():
+    parser = argparse.ArgumentParser()
+
+    # Common options.
+    parser.add_argument("-d", "--dataset", type=str, help="Path to the dataset")
+    parser.add_argument("-r", "--recon_path", type=str, default="reconstruction",
+                        help="Path to save reconstructed images")
+
+    parser.add_argument("-c", "--entropy-coder",
+                        choices=compressai.available_entropy_coders(),
+                        default=compressai.available_entropy_coders()[0],
+                        help="Entropy coder (default: %(default)s)")
+    parser.add_argument("--cuda", action="store_true", help="Enable CUDA")
+    parser.add_argument("--half", action="store_true", help="Convert model to half floating point (fp16)")
+    parser.add_argument("--entropy-estimation", action="store_true",
+                        help="Use evaluated entropy estimation (no entropy coding)")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
+    parser.add_argument("-p", "--path", dest="checkpoint_paths", type=str, nargs="*", required=True,
+                        help="Checkpoint paths")
+    parser.add_argument("--exp_name", type=str, required=True, help="Experiment name")
+
+    # Additional Options
+    parser.add_argument("--num_keep_patches", type=int, default=144, required=True,
+                        help="Number of patches to keep as input to the model")
+    parser.add_argument("--input_size", type=int, default=224, required=True, help="Size of the input image")
+
+    return parser
+
+
+def main(argv):
+    parser = setup_args()
+    args = parser.parse_args(argv)
+
+    filepaths = collect_images(args.dataset)
+    if len(filepaths) == 0:
+        print("Error: no images found in directory.", file=sys.stderr)
+        sys.exit(1)
+
+    compressai.set_entropy_coder(args.entropy_coder)
+
+    runs = args.check_paths
+    load_func = load_checkpoint
+
+    results = defaultdict(list)
+    for run in runs:
+        if args.verbose:
+            sys.stderr.write("\rEvaluating {run:s}".format("MCM", run=run))
+            sys.stderr.flush()
+
+        model = load_func(args.num_keep_patches, run)
+        if args.cuda and torch.cuda.is_available():
+            model = model.to("cuda")
+
+        model.update(force=True)
+
+        metrics = eval_model(
+            model,
+            args.recon_path,
+            args.dataset,
+            filepaths,
+            args,
+        )
+        for k, v in metrics.items():
+            results[k].append(v)
+
+    if args.verbose:
+        sys.stderr.write("\n")
+        sys.stderr.flush()
+
+    description = (
+        "entropy estimation" if args.entropy_estimation else args.entropy_coder
+    )
+    output = {
+        "name": args.architecture,
+        "description": f"Inference ({description})",
+        "results": results,
+    }
+
+    print(json.dumps(output, indent=2))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ utils/huffman.py
@@ -0,0 +1,169 @@
+import torch
+import heapq
+from collections import defaultdict
+
+
+class HuffmanCoding:
+    """
+    Huffman Coding class for compressing and decompressing PyTorch tensors.
+    """
+
+    class Node:
+        """
+        Node class to represent elements in the Huffman tree.
+        """
+
+        def __init__(self, value, freq):
+            """
+            Initialize a Node.
+
+            Args:
+                value: The value associated with the node.
+                freq: The frequency (count) of the value.
+            """
+            self.value = value
+            self.freq = freq
+            self.left = None
+            self.right = None
+
+        def __lt__(self, other):
+            """
+            Comparison method for sorting nodes in the heap.
+
+            Args:
+                other: Another node to compare with.
+
+            Returns:
+                True if this node has a lower frequency than the other.
+            """
+            return self.freq < other.freq
+
+    def __init__(self):
+        """
+        Initialize a HuffmanCoding instance.
+        """
+        self.heap = []  # Priority queue for building the Huffman tree
+        self.codes = {}  # Dictionary to store Huffman codes
+        self.reverse_mapping = {}  # Dictionary to map codes to values
+
+    def build_heap(self, tensor):
+        """
+        Build a heap of nodes based on the frequency of values in the tensor.
+
+        Args:
+            tensor: PyTorch tensor to be compressed.
+        """
+        frequency = defaultdict(int)
+        for value in tensor.view(-1):
+            value = int(value)
+            frequency[value] += 1
+
+        for value, freq in frequency.items():
+            node = self.Node(value, freq)
+            heapq.heappush(self.heap, node)
+
+    def build_tree(self):
+        """
+        Build the Huffman tree by merging nodes in the heap.
+        """
+        while len(self.heap) > 1:
+            node1 = heapq.heappop(self.heap)
+            node2 = heapq.heappop(self.heap)
+
+            merged_node = self.Node(None, node1.freq + node2.freq)
+            merged_node.left = node1
+            merged_node.right = node2
+
+            heapq.heappush(self.heap, merged_node)
+
+    def build_codes_helper(self, root, current_code):
+        """
+        Recursively build Huffman codes and the reverse mapping.
+
+        Args:
+            root: The current node in the tree.
+            current_code: The current Huffman code being constructed.
+        """
+        if root is None:
+            return
+
+        if root.value is not None:
+            self.codes[root.value] = current_code
+            self.reverse_mapping[current_code] = root.value
+
+        self.build_codes_helper(root.left, current_code + "0")
+        self.build_codes_helper(root.right, current_code + "1")
+
+    def build_codes(self):
+        """
+        Build Huffman codes and the reverse mapping after the tree is built.
+        """
+        root = heapq.heappop(self.heap)
+        current_code = ""
+        self.build_codes_helper(root, current_code)
+
+    def encode(self, tensor):
+        """
+        Encode the input tensor using Huffman codes.
+
+        Args:
+            tensor: PyTorch tensor to be encoded.
+
+        Returns:
+            The encoded text as a binary string.
+        """
+        encoded_text = ""
+        for value in tensor.view(-1):
+            value = int(value)
+            encoded_text += self.codes[value]
+        return encoded_text
+
+    def decode(self, encoded_text):
+        """
+        Decode the encoded text into a PyTorch tensor.
+
+        Args:
+            encoded_text: The encoded text as a binary string.
+
+        Returns:
+            The decoded PyTorch tensor.
+        """
+        current_code = ""
+        decoded_tensor = []
+        for bit in encoded_text:
+            current_code += bit
+            if current_code in self.reverse_mapping:
+                value = self.reverse_mapping[current_code]
+                decoded_tensor.append(value)
+                current_code = ""
+        return torch.tensor(decoded_tensor)
+
+    def compress(self, tensor):
+        """
+        Compress a PyTorch tensor using Huffman coding.
+
+        Args:
+            tensor: PyTorch tensor to be compressed.
+
+        Returns:
+            The compressed text as a binary string.
+        """
+        self.build_heap(tensor)
+        self.build_tree()
+        self.build_codes()
+        encoded_text = self.encode(tensor)
+        return encoded_text, tensor.device
+
+    def decompress(self, encoded_text, device):
+        """
+        Decompress the encoded text into a PyTorch tensor.
+
+        Args:
+            encoded_text: The encoded text as a binary string.
+            device: The torch device (cuda).
+
+        Returns:
+            The decompressed PyTorch tensor.
+        """
+        decoded_tensor = self.decode(encoded_text).to(device)
+        return decoded_tensor
