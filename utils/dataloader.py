66b4eb7ce15fb130b20725fc2190301520839c94
MINH KHANG TRAN <74235084+tmkhang1999@users.noreply.github.com>
2023-11-08 00:14:41 +0700

    Update dataloader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import torch
@@ -11,22 +12,25 @@ from utils.map import Division_Merge_Segmented, laplacian
 import cv2
 import numpy as np
 
-# Fix typo in the export name
-__all__ = ["CreateImageDataset", "get_image_dataset"]
+from timm.data import create_transform
+from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 
 
 class CreateImageDataset(Dataset):
-    def __init__(self, dataset_path, transform):
+    def __init__(self, is_train: bool, dataset_path: Path, transform):
         """
         Custom dataset for image data.
 
         Args:
-            dataset_path (str): Path to the dataset.
+            is_train (bool):
+            dataset_path (Path): Path to the dataset.
             transform (torchvision.transforms.Compose): Image transformations.
         """
         self.dataset_path = dataset_path
         self.transform = transform
-        self.imgs_path = sorted(Path(dataset_path).rglob("*.*"))
+        root = os.path.join(self.dataset_path, "train" if is_train else "val")
+
+        self.imgs_path = sorted(Path(root).rglob("."))
         assert len(self.imgs_path) > 0, f"No images found in {dataset_path}"
 
     def __len__(self):
@@ -34,7 +38,7 @@ class CreateImageDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.imgs_path[idx]
-        orig_img = Image.open(img_path).convert('RGB')
+        orig_img = Image.open(img_path).convert("RGB")
         orig_shape = orig_img.size
         total_score = calculate_patch_score(orig_img)
         img = self.transform(orig_img)
@@ -45,7 +49,7 @@ def calculate_patch_score(img):
     img = np.array(img, dtype=np.uint8)
     if img.shape[2] == 3:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+
     s_map = Division_Merge_Segmented(img, (224, 224))
     t_map = laplacian(img, (224, 224))
 
@@ -53,47 +57,52 @@ def calculate_patch_score(img):
     t_score = cal_patch_score(t_map)
 
     total_score = t_score * s_score
-    total_score = (total_score - total_score.min()) / (total_score.max() - total_score.min())
+    total_score = (total_score - total_score.min()) / (
+        total_score.max() - total_score.min()
+    )
     total_score = torch.tensor(total_score, dtype=torch.float32)
 
     return total_score
 
 
-def get_image_dataset(path: str, transform_cfg: dict = None) -> Dataset:
+def get_image_dataset(is_train: bool, dataset_path: Path, args) -> Dataset:
     """
     Get an image dataset.
 
     Args:
-        path (str) : Dataset path.
-        transform_cfg (dict, optional): Dictionary of transformation options.
+        is_train (bool):
+        dataset_path (str) : Dataset path.
+        args (dict, optional): config.
     """
-    transform = []
-
-    if transform_cfg:
-        if "crop" in transform_cfg:
-            # Random crop with padding
-            t = transforms.RandomCrop(
-                transform_cfg["crop"], pad_if_needed=True, padding_mode="reflect"
-            )
-            transform.append(t)
-
-        if transform_cfg.get("hflip", True):
-            # Random horizontal flip
-            t = transforms.RandomHorizontalFlip(p=0.5)
-            transform.append(t)
-
-    # Define a sequence of image transformations
-    transform = transforms.Compose(
-        [
-            transforms.Resize(224),
-            *transform,
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
-                                 0.229, 0.224, 0.225]),
-        ]
-    )
+    if is_train:
+        transform = create_transform(
+            input_size=args.input_size,
+            is_training=True,
+            color_jitter=args.color_jitter,
+            auto_augment=args.aa,
+            interpolation="bicubic",
+            re_prob=args.rebrob,
+            re_mode=args.remode,
+            re_count=args.recount,
+            mean=IMAGENET_DEFAULT_MEAN,
+            std=IMAGENET_DEFAULT_STD,
+        )
+    else:
+        t = []
+        if args.input_size <= 224:
+            crop_pct = 224 / 256
+        else:
+            crop_pct = 1
+        size = int(args.input_size / crop_pct)
+        t.append(
+            transforms.Resize(size, interpolation=Image.BICUBIC)
+        )  # to maintain same ratio w.r.t 224 images
+        t.append(transforms.CenterCrop(args.input_size))
+        t.append(transforms.ToTensor())
+        t.append(transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD))
+        transform = transforms.Compose(t)
 
     dataset = CreateImageDataset(
-        dataset_path=path, transform=transform
+        dataset_path=dataset_path, is_train=is_train, transform=transform
     )
     return dataset
